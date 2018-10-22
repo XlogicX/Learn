8b9c10cfc7a2e74a48cabd12de71467db45cad70
XlogicX <no.axiom@gmail.com>
2018-10-22 18:26:31 -0400

    Update LinuxAPI_04_time.s

--- ASM_ARM32/LinuxAPI_04_time.s
+++ ASM_ARM32/LinuxAPI_04_time.s
@@ -1,34 +1,34 @@
-;Example of some time API calls. API calls found in this example program:
-; 	time, gettimeofday, exit
-; High level description of what theis example program does:
-;	Get epoch time into memory pointer
-;	Get epoch time with microseconds into same memory pointer
+@Example of some time API calls. API calls found in this example program:
+@ 	time, gettimeofday, exit
+@ High level description of what theis example program does:
+@	Get epoch time into memory pointer
+@	Get epoch time with microseconds into same memory pointer
 
-section .text
-global _start
+.text
+.global _start
 
 _start:
 
-; Get the Epoch Time
-;------------------------------------------------------------------------------
-	mov 	eax, 13				;time
-    mov 	ebx, timeval		;timeval
-    int    	0x80
+@ Get the Epoch Time, this call fails: ENOSYS 38 Function not implemented
+@------------------------------------------------------------------------------
+	mov 	r7, #13		@time
+	ldr	r0, =timeval	@timeval
+    	swi	#0
 
-; Get the Epoch Time with microseconds
-;------------------------------------------------------------------------------
-	mov 	eax, 78				;gettimeofday
-    mov 	ebx, timeval		;timeval
-    mov		ecx, 0				;timezone, legacy argument, setting to null
-    int    	0x80
+@ Get the Epoch Time with microseconds (this works fine on ARM, better API anyway)
+@------------------------------------------------------------------------------
+	mov 	r7, #78		@gettimeofday
+	ldr	r0, =timeval	@timeval
+    	mov	r1, #0		@timezone, legacy argument, setting to null
+    	swi	#0
 
-; Exit
-;------------------------------------------------------------------------------
-	mov 	eax, 1
-	int 	0x80
+@ Exit
+@------------------------------------------------------------------------------
+	mov 	r7, #1
+	swi	#0
 
 
-section .bss
-timeval resb 8	;to keep track of where the heap started in the beginning
-				;	The first 4 bytes is the epoch time in seconds
-				;	The next 4 bytes is the remaining useconds
+.bss
+	.lcomm timeval, 8	@to keep track of where the heap started in the beginning
+					@ The first 4 bytes is the epoch time in seconds
+					@ The next 4 bytes is the remaining useconds
