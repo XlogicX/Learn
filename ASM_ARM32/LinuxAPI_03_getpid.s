916a353c9413f42231ea28a7d204bcd18bae4fa5
XlogicX <no.axiom@gmail.com>
2018-10-22 18:30:12 -0400

    Update LinuxAPI_03_getpid.s

--- ASM_ARM32/LinuxAPI_03_getpid.s
+++ ASM_ARM32/LinuxAPI_03_getpid.s
@@ -1,1 +1,35 @@
 
+@Example of the getpid API call. API calls found in this example program:
+@ 	getpid, getppid, exit
+@ High level description of what theis example program does:
+@	Uses the getpid API call to get it's current process ID.
+@	Stores this process ID number in memory (using pointer 'pid').
+@	Exits gracefully with exit().
+
+.text
+.global _start
+
+_start:
+
+@ Get Process ID
+@------------------------------------------------------------------------------
+	mov	r7, #20		@get Process ID of self, getpid()
+	swi	#0
+	ldr	r1, =pid	@Store it in a memory location
+	str	r0, [r1]	@...
+
+@ Get Parent Process ID
+@------------------------------------------------------------------------------
+	mov	r7, #64		@get the Parent Process ID of self getppid()
+	swi	#0
+	ldr	r1, =ppid	@Store it in a memory locatio
+	str	r0, [r1]	@...
+
+@ Exit program
+@------------------------------------------------------------------------------
+	mov	r7, #1
+	swi	#0
+
+.bss
+	.lcomm pid, 4
+	.lcomm ppid, 4
